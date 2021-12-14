75265da475ab9ecb1ab70e8a3c0cc77e51ef943a
zmccrystal <95671380+zmccrystal@users.noreply.github.com>
2021-12-14 14:40:00 -0500

    UdDare

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    var nounArr = ["your mom", "bird", "sandbox"];
+    var nounArr = ["momument", "hockey puck", "orange", "window", "television"];
 
     $("button").click(function() {
         $(".noun").each(function() {
@@ -9,4 +9,25 @@ $(document).ready(function() {
         });
         
     });
+    var adjectiveArr = ["skinny", "boring", "sensible", "dominant", "divine"];
+
+    $("button").click(function() {
+        $(".adjective").each(function() {
+            var randomIndex = Math.floor(Math.random() * adjectiveArr.length);
+            $(this).text(adjectiveArr[randomIndex]);
+        });
+
+      });
+  
+      var verbArr = ["stretch", "retire", "praise", "shout", "fight"];
+
+      $("button").click(function() {
+          $(".verb").each(function() {
+              var randomIndex = Math.floor(Math.random() * verbArr.length);
+              $(this).text(verbArr[randomIndex]);
+          });
+  
+        });
 });
+    
+
